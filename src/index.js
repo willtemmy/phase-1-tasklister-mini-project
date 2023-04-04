4f6f3d2f1792caf66b72f5e8d0484ce5309c7487
William Mtemi Mponzi <willbrian009@gmail.com>
2023-04-04 19:05:04 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.new-task-description.value)
+    form.reset()
+  })
+})
+
+function buildToDo(todo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+};
